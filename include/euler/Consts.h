a1e22776343dbca0862e05f0d3c24a8819b06a97
nedveri <nedveri.dev@gmail.com>
2026-07-08 17:00:22 +0300

    Initial commit

---
+++ include/euler/Consts.h
@@ -0,0 +1,15 @@
+/*
+ * Consts.h
+ *
+ *  Created on: Jul 8, 2026
+ *      Author: ywiuus
+ */
+
+#ifndef INCLUDE_CONSTS_H_
+#define INCLUDE_CONSTS_H_
+
+
+
+
+
+#endif /* INCLUDE_CONSTS_H_ */
---
+++ src/Consts.cpp
@@ -0,0 +1,10 @@
+/*
+ * Consts.cpp
+ *
+ *  Created on: Jul 8, 2026
+ *      Author: ywiuus
+ */
+
+
+
+
